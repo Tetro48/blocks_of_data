b46191659b13188315683f0b4ca55137b62f21c0
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2022-02-03 13:04:55 +0700

    Mostly working internals now, added README. Note: NativeArrays in Entities break my mind in a bad way. How do I manage that???

--- Assets/Components/PlayerComponent.cs
+++ Assets/Components/PlayerComponent.cs
@@ -6,10 +6,12 @@ using Unity.Mathematics;
 [Serializable, GenerateAuthoringComponent]
 public struct PlayerComponent : IComponentData
 {
-    public NativeArray<int3> activePiece;
-    public NativeArray<Byte> boardState;
+    public NativeArray<int2> activePiece;
+    public int textureID;
+    public NativeArray<byte> boardState;
     public int lines, frames;
     public bool isControllable;
-    public float LockDelay, LockDelayf, DAS, SDF, ARE, AREf, AREline, lineDelay, gravity;
+    public float LockDelay, LockDelayf, DAS, SDF, ARE, AREf, AREline, lineDelay, gravity, fallenTiles;
+    public int2 posToMove;
     public int lineDelayf;
 }
--- Assets/Systems/PlayerSystem.cs
+++ Assets/Systems/PlayerSystem.cs
@@ -11,33 +11,42 @@ public class PlayerSystem : SystemBase
     {
         float deltaTime = Time.DeltaTime;
         
-        Entities.ForEach((PlayerComponent player) => {
+        Entities.ForEach((ref PlayerComponent player) => {
             NativeArray<bool> isLineFull = new NativeArray<bool>(40, Allocator.TempJob);
+            player.fallenTiles += player.gravity * deltaTime;
+            player.posToMove.y -= (int)math.floor(player.fallenTiles);
+            for (int i = 0; i < player.activePiece.Length; i++)
+            {
+                player.activePiece[i] += player.posToMove;
+            }
+            player.fallenTiles -= math.floor(player.fallenTiles);
             for (int y = 0; y < player.boardState.Length / 10; y++)
             {
                 isLineFull[y] = true;
                 for (int i = 0; i < 10; i++)
                 {
-                    if (player.boardState[i + (y * 10)] > 127)
+                    if (player.boardState[i + (y * 10)] < 128)
                     {
-                        isLineFull[y] = false;
-                        break;
+                        continue;
                     }
+                    isLineFull[y] = false;
+                    break;
                 }
-                if (isLineFull[y])
+                if (!isLineFull[y])
                 {
-                    int count = player.boardState.Length;
-                    for (int i = y * 10; i < count - 1; i++)
-                    {
-                        player.boardState[i+10] = player.boardState[i];
-                    }
-                    for (int i = 0; i < 10; i++)
-                    {
-                        player.boardState[count - i] = 128;
-                    }
+                    continue;
+                }
+                int count = player.boardState.Length;
+                for (int i = 0; i < 10; i++)
+                {
+                    player.boardState[count - i] = 128;
+                }
+                for (int i = y * 10; i < count - 1; i++)
+                {
+                    player.boardState[i] = player.boardState[i+10];
                 }
             }
             isLineFull.Dispose();
-        }).Schedule();
+        }).ScheduleParallel();
     }
 }
